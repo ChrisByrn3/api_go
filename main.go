0914ae17a4e1f59f1682ea6d6c10bd5965ab3e15
ChrisByrn3 <chris.byrn3@gmail.com>
2023-05-05 09:25:16 +0100

    able to search for item by id

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ var albums = []album{
 func main(){
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:3000")
@@ -45,4 +46,18 @@ if err := c.BindJSON(&newAlbum); err != nil {
 //Add the album to the slice
 albums = append(albums, newAlbum)
 c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID matches the id
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// loop over list of albums, looking for an album that matches the parameter
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
